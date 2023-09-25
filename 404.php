43c0c0cd502a2265c21a7d0cf6cccd1a6f17c0a4
hubert20 <hubert@tomedia.pl>
2023-09-25 21:36:17 +0200

    404

--- 404.php
+++ 404.php
@@ -12,11 +12,6 @@ get_header(); ?>
 
 <main id="main" class="page-404 pt-5" role="main">
     <div class="container py-5">
-        <div class="row justify-content-center">
-            <div class="col-8 col-lg-5 mt-lg-5 text-center pt-lg-4">
-                <img src="" alt="Error" class="img-fluid mb-4 mb-lg-5">
-            </div>
-        </div>
         <p class="mb-3 mb-lg-5 text-center teko-font standard-title-5 text-green">
             Przepraszamy za kłopot. <br> A korzystając z okazji chcemy Ci podziękować, że jesteś z nami.
         </p>
