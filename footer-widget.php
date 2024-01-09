7cb35bdb4e56e00f3fcde6e51a5e7705c1b442a6
hubert20 <hubert@tomedia.pl>
2024-01-09 10:15:57 +0100

    Update footer-widget.php

--- footer-widget.php
+++ footer-widget.php
@@ -3,7 +3,7 @@
     <div class="container py-4">
         <hr>
         <div class="text-center">
-            <a href="https://www.wiosna.org.pl/regulamin-serwisu/" title="Regulamin" target="_blank" rel="noopener" class="text-dark"><small>Regulamin</small></a>&nbsp;&nbsp;&nbsp;<a href="https://www.wiosna.org.pl/polityka-prywatnosci/" title="Polityka prywatności" target="_blank" rel="noopener" class="text-dark"><small>Polityka prywatności</small></a> &nbsp;&nbsp;&nbsp;<small>Stowarzyszenie WIOSNA Copyright 2009-2023</small>
+            <a href="https://www.wiosna.org.pl/regulamin-serwisu/" title="Regulamin" target="_blank" rel="noopener" class="text-dark"><small>Regulamin</small></a>&nbsp;&nbsp;&nbsp;<a href="https://www.wiosna.org.pl/polityka-prywatnosci/" title="Polityka prywatności" target="_blank" rel="noopener" class="text-dark"><small>Polityka prywatności</small></a> &nbsp;&nbsp;&nbsp;<small>Stowarzyszenie WIOSNA Copyright 2009-2024</small>
         </div>
     </div>
 </div>
