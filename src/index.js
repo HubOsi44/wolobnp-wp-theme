2df73fb558602bc909454b6b3a66025b54d2e445
hubert20 <hubert@tomedia.pl>
2024-03-18 13:22:25 +0100

    Added  script in modal

--- src/index.js
+++ src/index.js
@@ -2,16 +2,49 @@ require("bootstrap");
 import 'owl.carousel';
 import './custom.javascript';
 import { Modal } from 'bootstrap';
+// const myModal = new Modal(document.getElementById('modalMaterials'));
+// const myBtnhidemodal = document.getElementById('closeModal');
+// window.addEventListener('DOMContentLoaded', () => {
+//     myModal.show();
+// });
+
+// var wpcf7Elm = document.querySelector('.wpcf7');
+
+// wpcf7Elm.addEventListener('wpcf7mailfailed', function (event) {
+//     setTimeout(function () {
+//         myModal.hide();
+//     }, 3000);
+// }, false);
+
+// Hide modal after send request with option cookie
 const myModal = new Modal(document.getElementById('modalMaterials'));
-const myBtnhidemodal = document.getElementById('closeModal');
+
+
+// Funkcja do sprawdzania, czy istnieje ciasteczko o nazwie "modalShown"
+function checkModalCookie() {
+    return document.cookie.split(';').some((item) => item.trim().startsWith('modalShown='));
+}
+
+// Funkcja do ustawiania ciasteczka o nazwie "modalShown" na wartość "true" na 7 dni
+function setModalCookie() {
+    const d = new Date();
+    d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000)); // Ciasteczko ważne przez 7 dni
+    const expires = "expires="+ d.toUTCString();
+    document.cookie = "modalShown=true;" + expires + ";path=/";
+}
+
+// Sprawdzanie, czy modal powinien zostać pokazany na podstawie ciasteczka
 window.addEventListener('DOMContentLoaded', () => {
-    myModal.show();
+    if (!checkModalCookie()) {
+        myModal.show();
+    }
 });
 
-var wpcf7Elm = document.querySelector('.wpcf7');
-
+// Ukrycie modala po naciśnięciu przycisku zamykającego
+const wpcf7Elm = document.querySelector('.wpcf7');
 wpcf7Elm.addEventListener('wpcf7mailfailed', function (event) {
     setTimeout(function () {
         myModal.hide();
+        setModalCookie(); // Ustawienie ciasteczka po wystąpieniu błędu
     }, 3000);
 }, false);
--- templates/materials.php
+++ templates/materials.php
@@ -18,7 +18,28 @@ endwhile;
     <div class="modal-dialog modal-dialog-centered align-items-center modal-xl" role="document">
         <div class="modal-content border-0">
             <div class="modal-body">
-                <?= apply_shortcodes('[contact-form-7 id="d41eb69" title="Formularz kontaktowy" html_id="zgloszenie" html_name="zgloszenie"]'); ?>
+                <div class="row">
+                    <div class="col-lg-6 d-flex flex-column justify-content-center">
+                        <h2 class="mb-3 mb-lg-4 fw-light lh-12">
+                            <span class="fw-bold standard-title-4 d-inline-block lh-12 mb-2 mb-lg-3 text-red text-center">
+                                Zdobądz dostęp do eksluzywnej zawartości materiałów o wolontariacie.
+                            </span><br>
+                            <span class="standard-title-6 lh-13 d-inline-block text-center">
+                                To bardzo proste, wystarczy, że wyslesz do nas swoje dane: imie oraz adres-email,
+                                dzięki temu zdobędziesz dostęp do wielu atrakcyjnych i eksluzywnych materiałów,
+                                pogłebisz wiedze na temat wolontariatu pracowniczego. 
+                            </span>
+                        </h2>
+                    </div>
+                    <div class="col-lg-1 text-center">
+                        <div class="mx-auto h-100 d-none d-lg-inline-block" style="width: 2px;background: rgba(0,0,0, .1);"></div>
+                    </div>
+                    <div class="col-lg-5">
+                        <div class="pe-lg-3 py-lg-3">
+                            <?= apply_shortcodes('[contact-form-7 id="d41eb69" title="Formularz kontaktowy" html_id="zgloszenie" html_name="zgloszenie"]'); ?>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
