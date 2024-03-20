c64f9f134783fddbfc89d646138dc655ab9849f0
hubert20 <hubert@tomedia.pl>
2024-03-20 13:29:30 +0100

    Modal Materials structure srcipt+ style

--- src/index.js
+++ src/index.js
@@ -2,52 +2,40 @@ require("bootstrap");
 import 'owl.carousel';
 import './custom.javascript';
 import { Modal } from 'bootstrap';
-// const myModal = new Modal(document.getElementById('modalMaterials'));
-// const myBtnhidemodal = document.getElementById('closeModal');
-// window.addEventListener('DOMContentLoaded', () => {
-//     myModal.show();
-// });
 
-// var wpcf7Elm = document.querySelector('.wpcf7');
+const modalMat = document.getElementById('modalMaterials');
+if (modalMat) {
+    // Hide modal after send request with option cookie
+    const myModal = new Modal(document.getElementById('modalMaterials'));
 
-// wpcf7Elm.addEventListener('wpcf7mailfailed', function (event) {
-//     setTimeout(function () {
-//         myModal.hide();
-//     }, 3000);
-// }, false);
-
-// Hide modal after send request with option cookie
-const myModal = new Modal(document.getElementById('modalMaterials'));
-
-
-// Funkcja do sprawdzania, czy istnieje ciasteczko o nazwie "modalShown"
-function checkModalCookie() {
-    return document.cookie.split(';').some((item) => item.trim().startsWith('modalShown='));
-}
-
-// Funkcja do ustawiania ciasteczka o nazwie "modalShown" na wartość "true" na 7 dni
-function setModalCookie() {
-    const d = new Date();
-    d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000)); // Ciasteczko ważne przez 7 dni
-    const expires = "expires="+ d.toUTCString();
-    document.cookie = "modalShown=true;" + expires + ";path=/";
-}
-
-// Sprawdzanie, czy modal powinien zostać pokazany na podstawie ciasteczka
-var contentbeforeModal = document.querySelector('.content-before-modal');
-window.addEventListener('DOMContentLoaded', () => {
-    if (!checkModalCookie()) {
-        myModal.show();
-        contentbeforeModal.classList.add('blurred-content');
+    // Funkcja do sprawdzania, czy istnieje ciasteczko o nazwie "modalShown"
+    function checkModalCookie() {
+        return document.cookie.split(';').some((item) => item.trim().startsWith('modalShown='));
     }
-});
+    // Funkcja do ustawiania ciasteczka o nazwie "modalShown" na wartość "true" na 7 dni
+    function setModalCookie() {
+        const d = new Date();
+        d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + d.toUTCString();
+        document.cookie = "modalShown=true;" + expires + ";path=/";
+    }
 
-// Ukrycie modala po naciśnięciu przycisku zamykającego
-const wpcf7Elm = document.querySelector('.wpcf7');
-wpcf7Elm.addEventListener('wpcf7mailfailed', function (event) {
-    setTimeout(function () {
-        myModal.hide();
-        setModalCookie(); // Ustawienie ciasteczka po wystąpieniu błędu
-        contentbeforeModal.classList.remove('blurred-content');
-    }, 3000);
-}, false);
+    // Sprawdzanie, czy modal powinien zostać pokazany na podstawie ciasteczka
+    var contentbeforeModal = document.querySelector('.content-before-modal');
+    window.addEventListener('DOMContentLoaded', () => {
+        if (!checkModalCookie()) {
+            myModal.show();
+            contentbeforeModal.classList.add('blurred-content');
+        }
+    });
+
+    // Ukrycie modala po naciśnięciu przycisku zamykającego
+    const wpcf7Elm = document.querySelector('.wpcf7');
+    wpcf7Elm.addEventListener('wpcf7mailsent', function (event) {
+        setTimeout(function () {
+            myModal.hide();
+            setModalCookie();
+            contentbeforeModal.classList.remove('blurred-content');
+        }, 3000);
+    }, false);
+}
--- templates/materials.php
+++ templates/materials.php
@@ -5,40 +5,35 @@ if (!defined('ABSPATH')) exit;
 
 get_header();
 
-?>
+$top_bg = get_field('top_bg');
+$cnt_modal = get_field('cnt_modal');
 
-<div class="content-before-modal">
+?>
+<main id="main" class="content-before-modal" role="main">
+    <section class="d-flex align-items-end page-header-top" style="background-image: url('<?= $top_bg; ?>');">
+        <div class="container">
+            <h1 class="text-center mb-4 mb-lg-5 fw-bold standard-title-1 text-white"><span class="bg-green-shadow"><?php the_title(); ?></span></h1>
+        </div>
+    </section>
     <?php
     while (have_posts()) : the_post();
         the_content(__('Continue reading <span class="meta-nav">&rarr;</span>', 'wolobnp-wp-theme'));
     endwhile;
     ?>
-</div>
+</main>
 
-<!-- Modal Film -->
+<!-- Modal with form -->
 <div class="modal modal-materials fade" id="modalMaterials" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
     <div class="modal-dialog modal-dialog-centered align-items-center modal-xl" role="document">
         <div class="modal-content border-0">
             <div class="modal-body">
                 <div class="row">
-                    <div class="col-lg-6 d-flex flex-column justify-content-center">
-                        <h2 class="mb-3 mb-lg-4 fw-light lh-12">
-                            <span class="fw-bold standard-title-4 d-inline-block lh-12 mb-2 mb-lg-3 text-red text-center">
-                                Zdobądz dostęp do eksluzywnej zawartości materiałów o wolontariacie.
-                            </span><br>
-                            <span class="standard-title-6 lh-13 d-inline-block text-center">
-                                To bardzo proste, wystarczy, że wyslesz do nas swoje dane: imie oraz adres-email,
-                                dzięki temu zdobędziesz dostęp do wielu atrakcyjnych i eksluzywnych materiałów,
-                                pogłebisz wiedze na temat wolontariatu pracowniczego.
-                            </span>
-                        </h2>
+                    <div class="col-lg-8 d-flex flex-column justify-content-center">
+                        <?= $cnt_modal; ?>
                     </div>
-                    <div class="col-lg-1 text-center">
-                        <div class="mx-auto h-100 d-none d-lg-inline-block" style="width: 2px;background: rgba(0,0,0, .1);"></div>
-                    </div>
-                    <div class="col-lg-5">
-                        <div class="pe-lg-3 py-lg-3">
-                            <?= apply_shortcodes('[contact-form-7 id="d41eb69" title="Formularz kontaktowy" html_id="zgloszenie" html_name="zgloszenie"]'); ?>
+                    <div class="col-lg-4">
+                        <div class="bg-gray-light p-3" style="border-radius: 10px;">
+                            <?= apply_shortcodes('[contact-form-7 id="a39675c" title="Formularz materials" html_id="zgloszenie" html_name="zgloszenie"]'); ?>
                         </div>
                     </div>
                 </div>
